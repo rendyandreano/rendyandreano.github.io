18f166c2f8737e67876e21ec60a605c0678aaa27
rendyandreano <rendyandrr@gmail.com>
2026-06-15 20:27:51 +0700

    edit about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
-import { Code2, MonitorCloud, TabletSmartphone, PencilRuler } from "lucide-react";
+import { Code2, TabletSmartphone, PencilRuler } from "lucide-react";
 
 const highlights = [
   { icon: Code2, label: "Full Stack Dev" },
-  { icon: MonitorCloud, label: "SaaS" },
+  { icon: TabletSmartphone, label: "SaaS" },
   { icon: TabletSmartphone , label: "Mobile Dev" },
   { icon: PencilRuler, label: "UI/UX Design" },
 ];
