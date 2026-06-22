57bd6f0d5ae3edc3baa513a86d2db89be4b011aa
rendyandreano <rendyandrr@gmail.com>
2026-06-22 21:50:43 +0700

    edit error index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Timeline from "@/components/Timeline";
-import Sertificatios from "@/components/Sertificatios";
+import Sertifications from "@/components/Sertifications";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import { useEffect } from "react";
@@ -33,7 +33,7 @@ const Index = () => {
       <Skills />
       <Projects />
       <Timeline />
-      <Sertificatios/>
+      <Sertifications/>
       <Contact />
       <Footer />
     </div>
